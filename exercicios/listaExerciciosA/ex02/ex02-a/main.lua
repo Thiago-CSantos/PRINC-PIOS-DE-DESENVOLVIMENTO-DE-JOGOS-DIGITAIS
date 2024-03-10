77885bde6dcac1feb6beb0e9c0b98c63bcbbd1a5
Thiago-CSantos <thiagocarretero433@gmail.com>
2024-03-10 16:07:44 -0300

    ex02-a

---
+++ exercicios/listaExerciciosA/ex02/ex02-a/main.lua
@@ -0,0 +1,16 @@
+function love.load()
+      love.graphics.setColor(1,1,1,1)
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+      -- Tela x-800 y-600
+      -- love.graphics.print(love.graphics.getHeight())
+      -- love.graphics.print(love.graphics.getWidth())
+
+      love.graphics.line(50,250, 150,200, 150,150, 200,100, 200,50, 150,50, 100,100, 50,150, 50,250)
+      
+end
