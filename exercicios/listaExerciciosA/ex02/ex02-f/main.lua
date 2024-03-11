41d21bb512cee341a22a46cf122d738a17650e64
Thiago-CSantos <thiagocarretero433@gmail.com>
2024-03-10 21:57:39 -0300

    não deu certo

--- exercicios/listaExerciciosA/ex02/ex02-f/main.lua
+++ exercicios/listaExerciciosA/ex02/ex02-f/main.lua
@@ -13,6 +13,16 @@ function love.draw()
       -- Tela x-800 y-600
       -- love.graphics.print(centro_x) --400
       -- love.graphics.print(centro_y) --300
+      curva = love.math.newBezierCurve(200,400, 130,350, 200,300)
+      curva2 = love.math.newBezierCurve(500,200, 510,200, 500,300  )
 
-      
+      love.graphics.circle("line",200, 250, 50)
+      love.graphics.line(200, 200, 500, 200)
+
+      love.graphics.circle("line",500, 350, 50)
+      love.graphics.line(200,400, 500,400)
+
+      love.graphics.line(curva:render())
+      love.graphics.line(curva2:render())
+
 end
